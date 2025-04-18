bab9a0fb5e09f978bd7b72badc25ef1556a49396
PritamBhowmik123 <bhowmikpritam04@gmail.com>
2025-04-18 14:37:30 +0530

    home page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
     <div>
-      app
+      <Home/>
     </div>
   )
 }
---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,44 @@
+import home_icon from './home.png'
+import search_icon from './search.png'
+import plus_icon from './plus.png'
+import arrow from './arrow.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import history from './history.png'
+import menu from './menu.png'
+import news from './news.png'
+import notification from './notification.png'
+import show_more from './show-more.png'
+import userIcon from './user_profile.jpg'
+import crops1 from './crops1.jpg'
+import crops2 from './crops2.jpg'
+import crops3 from './crops3.jpg'
+import crops4 from './crops4.jpg'
+import seeds from './seeds.jpg'
+import fertilizer from './fertilizer.jpg'
+import pests from './pestisides.jpg'
+import tools from './tools.jpg'
+
+export const assets = {
+    home_icon,
+    search_icon,
+    plus_icon,
+    arrow,
+    arrow_left,
+    arrow_right,
+    history,
+    menu,
+    news,
+    notification,
+    show_more,
+    userIcon,
+    crops1,
+    crops2,
+    crops3,
+    crops4,
+    seeds,
+    fertilizer,
+    pests,
+    tools,
+}
+
