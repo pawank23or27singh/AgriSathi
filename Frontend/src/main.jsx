29c2478c2423c0fad42e78fabcf032e4a2ac0e85
PritamBhowmik123 <bhowmikpritam04@gmail.com>
2025-04-19 23:32:31 +0530

    cart context added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './Context/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
   </StrictMode>,
 )
